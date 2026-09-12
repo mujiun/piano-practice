701c7bc4d279ce8c6d83ebabfcd8a7bd19678970
mujiun <mujiun@users.noreply.github.com>
2026-09-12 16:53:52 -0700

    Debussy: source Arabesque No.1 & No.2 (PD 1904 Durand/Elkan-Vogel scan)

--- library/debussy/img/manifest.js
+++ library/debussy/img/manifest.js
@@ -1,2 +1,2 @@
-window.SCORE_IMG = window.SCORE_IMG || {};
-Object.assign(window.SCORE_IMG, {"debussy-g74": 1, "debussy-g75": 1, "deb-clair": 1, "debussy-g76": 1, "debussy-g88": 2, "debussy-g87": 8, "debussy-g89": 7});
+// Debussy page-strip counts (public-domain PDF)
+window.SCORE_IMG = Object.assign(window.SCORE_IMG||{}, {"debussy-g74":1,"debussy-g75":1,"deb-clair":1,"debussy-g76":1,"debussy-g88":2,"debussy-g87":8,"debussy-g89":7,"deb-arab1":5,"debussy-g77":6});
