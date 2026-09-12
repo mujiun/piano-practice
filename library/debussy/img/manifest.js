ce6b1c0ba9affeab81a1a1e5f4fe1bb811e54b31
mujiun <mujiun@users.noreply.github.com>
2026-09-11 22:45:55 -0700

    Debussy scores: 7 pieces as page-strip images (Suite bergamasque, Pour le piano, L'Isle joyeuse); generalize image path per composer; Capacitor project scaffold

---
+++ library/debussy/img/manifest.js
@@ -0,0 +1,2 @@
+window.SCORE_IMG = window.SCORE_IMG || {};
+Object.assign(window.SCORE_IMG, {"debussy-g74": 1, "debussy-g75": 1, "deb-clair": 1, "debussy-g76": 1, "debussy-g88": 2, "debussy-g87": 8, "debussy-g89": 7});
